ba86e3ba9b91d52f96da2961791e2d7da042a54c
Sam Ellis <samellis@Sams-MacBook-Pro>
2023-12-22 15:29:15 +0000

    Added carbon intesity

--- src/energy.py
+++ src/energy.py
@@ -8,6 +8,14 @@ import xml.etree.ElementTree as ET
 
 # https://www.energydashboard.co.uk/data
 
+# Get carbon intensity
+# https://carbonintensity.org.uk/
+# https://carbon-intensity.github.io/api-definitions/#get-intensity
+c_url = "https://api.carbonintensity.org.uk/intensity"
+response = requests.get(c_url)
+content = response.json()
+carbon_intensity = content["data"][0]["intensity"]["actual"]
+carbon_intensity_index = content["data"][0]["intensity"]["index"]
 # Get Solar Data
 # https://www.solar.sheffield.ac.uk/pvlive/
 pv_url = "https://api.solar.sheffield.ac.uk/pvlive/api/v4/gsp/0"
@@ -25,7 +33,6 @@ root = ET.fromstring(xml_response)
 
 
 updated_at = root[0].attrib["AT"]
-print(updated_at)
 
 fuel_types = []
 consumption = []
@@ -33,7 +40,7 @@ for child in root[0]:
     fuel_types.append(child.attrib["TYPE"])
     consumption.append(float(child.attrib["VAL"]))
 
-fuels = pd.Series(fuel_types, name="Source")
+fuels = pd.Series(fuel_types, name="Fuel Type")
 values = pd.Series(consumption, name="Consumption")
 imports = [
     "INTFR",
@@ -47,29 +54,38 @@ imports = [
     "INTVKL",
 ]
 gas = ["OCGT", "CCGT"]
-hydro = ["PS", "NPSHYD"]
+hydro = ["NPSHYD"]
+pumped = ["PS"]
 
 df = pd.DataFrame(data=[consumption], columns=fuels)
 total1 = df.sum(axis=1)[0]
-print(df)
-print(total1)
+
 df["Imports"] = df[imports].sum(axis=1)
 df["Gas"] = df[gas].sum(axis=1)
 df["Hydro"] = df[hydro].sum(axis=1)
+df["Pumped Storage"] = df[pumped].sum(axis=1)
 df["Solar"] = solar
-drop_columns = imports + gas + hydro
+drop_columns = imports + gas + hydro + pumped
 df.drop(columns=drop_columns, inplace=True)
 
 total2 = df.sum(axis=1)[0]
-print(total2)
+
 
 df_proportion = pd.DataFrame(
     data=[[round(100 * df[col].sum() / total1, 2) for col in df.columns]],
     columns=df.columns,
 )
-print(f"Total generation: {total2}")
-print(df)
-print(df_proportion)
+
+df_all = pd.concat([df, df_proportion], axis=0)
+df_t = df_all.transpose()
+df_t.columns = ["Amount", "Percentage"]
+
+print(f"Live Fuel Mix {updated_at}:")
+print(df_t)
+print(f"Total generation is {round(total2/1000,2)} GW")
+print(f"Carbon intensity is {carbon_intensity} gCO2/kWh ({carbon_intensity_index})")
 
 # from grid.iamkate: Onshore wind turbines in England and Wales (and some in Scotland) are connected to the local distribution network rather than the national transmission network, so their reported power generation is an estimate from National Grid ESO, based on weather conditions and observed transmission network demand.
 # offshore wind turbines are connected to transmission network so power gen is measured directly
+
+# missing wind: 18766-14083 = 4683 at 15:08
